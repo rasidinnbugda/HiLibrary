e068568d6e1083779309f04b2d79aba8e88d396b
Raşidin Buğda <rasidinnbugda@gmail.com>
2023-07-19 18:12:27 +0300

    Change Langugae System

---
+++ languages/en.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang = array(
+    // Header
+    "home" => "Home",
+    "documentations" => "Documentations",
+    "userMenu1" => "Admin Settings",
+    "userMenu2" => "Log",
+    "userMenu3" => "Log Out",
+    // Sidebar
+    "myLibraries" => "My Libraries",
+    "management" => "Management",
+    "libraries" => "Libraries",
+    // Main
+    
+);
+
+?>
